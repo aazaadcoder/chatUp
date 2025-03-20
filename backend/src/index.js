c7c7e8b9e57c974d740a086f13decf31fc8cb7ce
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-03-20 12:52:29 +0530

    add changes to the code to make code ready for deployment and we will server the frontedn from the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,11 +7,12 @@ import { connectDB } from "../lib/db.js";
 import messageRouter from "./routes/message.route.js";
 import cors from "cors";
 import { app, io, server } from "../lib/socket.js";
-
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
 app.use(express.json({limit : '50mb'}));
@@ -26,6 +27,15 @@ app.use(cors(
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
+
 
 app.get("/", (req,  res)=> {
   return res.json({message : "hello from server"})
